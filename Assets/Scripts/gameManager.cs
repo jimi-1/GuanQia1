f721ecd6cf6a27696b053da5e382cbaad007abe9
zhanghaixin123 <1208655757@qq.com>
2019-03-26 20:36:10 +0800

    第一个关卡

---
+++ Assets/Scripts/gameManager.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gameManager : MonoBehaviour {
+
+    public player thePlayer;
+
+    void Start () {
+
+        //在游戏运行时，首先遍历一遍PlayerController脚本
+        player thePlayer = FindObjectOfType<player>();
+
+    }
+    
+    // Update is called once per frame
+    void Update () {
+        
+    }
+
+
+    /// 调用方法Respawn开启携程方法RespawnCo，完成等待复活事件
+    public void Respawn()
+    {
+        //开启携程RespawnCo
+        StartCoroutine("RespawnCo");
+        
+    }
+
+
+    /// <summary>
+    /// 携程，等待X秒后复活角色
+    /// </summary>
+    /// <returns></returns>
+    public IEnumerator RespawnCo()
+    {
+        
+        //隐藏角色
+        thePlayer.gameObject.SetActive(false);
+        //等待X秒后执行
+        
+        yield return new WaitForSeconds(2);
+        //把复活碰撞点的位置传给角色，让角色在复活碰撞点复活
+        
+        thePlayer.transform.position = thePlayer.RespawnPosition;
+        //显示角色
+        
+        thePlayer.gameObject.SetActive(true);
+    }
+
+}
